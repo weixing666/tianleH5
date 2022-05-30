08cfb6c4754951aa0ad2dd06669e073e1398a9fb
weixing <2804057621@qq.con>
2022-05-30 19:23:25 +0800

    perf : vite

---
+++ src/views/test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <button @click="handleClick">button 获取用户</button>
+    </div>
+</template>
+
+<script>
+    import axios from "axios"
+    export default {
+        methods:{
+            async handleClick(){
+                // 代理 /myapi/users  最终地址会被合并成:   http://localhost:4000/users
+                let {data} = await axios.get('/myapi/users')
+                console.log(data)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,55 @@
-import { createVuePlugin } from 'vite-plugin-vue2'
+import { createVuePlugin} from 'vite-plugin-vue2'
+import { defineConfig } from 'vite'
+import commonjs from "rollup-plugin-commonjs";
+import externalGlobals from "rollup-plugin-external-globals";
 
-export default {
-  plugins: [createVuePlugin()]
-}
+export default defineConfig({
+    plugins: [createVuePlugin()],
+    // 代理跨域请求
+    server: {
+        // 如 /myapi/users 会被转发 http://localhost:4000/users  , 开发情况下一般会设置很多个
+        proxy: {
+            '/myapi': {
+                target: 'http://localhost:3001/', // 代理域名地址
+                changeOrigin: true, // true 保留请求源头 请求头：Referer
+                rewrite: (path) => path.replace(/^\/myapi/, '') // 重写url路径
+            },
+        }
+    },
+    // 配置别名 
+    resolve: {
+        // 设置路径别名
+        alias: {
+            '@': '/src',
+            '@api': '/src/api'
+        },
+        // 忽略后缀，不建议忽略.vue
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+    },
+    // 针对生产环境（线上环境）
+    build: {
+        minify: "terser",
+        terserOptions: {
+            // 去除console和debugg
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            }
+        },
+        // 小于此阈值的导入或引用资源将内联为 base64 编码， 以避免额外的 http 请求。 设置为 0 可以完全禁用此项。 默认4096 (4kb)
+        // 阈值不要设置太大，太大解码很耗性能
+        assetsInlineLimit: 4096 * 2, // 8kb
+        // 配置cdn加速
+        rollupOptions: {
+            external: ["axios", "moment"],
+            plugins: [
+                commonjs(),
+                externalGlobals({
+                    axios: "axios",
+                    moment: "moment",
+                }),
+            ],
+        },
+    }
+
+})
